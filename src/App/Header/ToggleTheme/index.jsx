74c7e86795a74693b7a0fe2b723963498d35264d
Krystian Żybura <Krystian12324444@gmail.com>
2023-07-31 18:38:38 +0200

    ToggleTheme upgraded

--- src/App/Header/ToggleTheme/index.jsx
+++ src/App/Header/ToggleTheme/index.jsx
@@ -1,15 +1,24 @@
-import {
-  DarkThemeStatus,
-  ToggleThemeButton,
-  ToggleThemeContainer,
-} from "./styled";
+import { useState } from "react";
 
-const ToggleTheme = () => (
-  <ToggleThemeContainer>
-    <DarkThemeStatus>{`Dark mode off`}</DarkThemeStatus>
-    <ToggleThemeButton>
-      <img src="/light.svg" alt="toggle theme button" />
-    </ToggleThemeButton>
-  </ToggleThemeContainer>
-);
+import { SpecialText } from "../../common/SpecialText";
+import { ToggleThemeButton, ToggleThemeContainer } from "./styled";
+
+const ToggleTheme = () => {
+  const [darkTheme, setDarkTheme] = useState(false);
+
+  return (
+    <ToggleThemeContainer>
+      <SpecialText>{`Dark mode off`}</SpecialText>
+      <ToggleThemeButton
+        onClick={() => setDarkTheme((darkTheme) => !darkTheme)}
+      >
+        <img
+          src={`/${darkTheme ? "dark" : "light"}.svg`}
+          alt="toggle theme button"
+        />
+      </ToggleThemeButton>
+    </ToggleThemeContainer>
+  );
+};
+
 export default ToggleTheme;
