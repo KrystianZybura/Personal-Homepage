bc41aa87672064cc2d52893ab9f60bef074342c1
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-04 16:16:23 +0200

    corrected section styled

--- src/App/common/Section/styled.js
+++ src/App/common/Section/styled.js
@@ -1,12 +1,14 @@
 import { styled } from "styled-components";
 
-const StyledSection = styled.div`
+const StyledSection = styled.section`
   background: ${({ theme }) => theme.sectionBackround};
 `;
 
 const SectionHeader = styled.h2`
   font-size: 30px;
   font-weight: 900;
+  padding-bottom: 15px;
+  border-bottom: 1px solid grey;
 `;
 
 const SectionContainer = styled.div`
