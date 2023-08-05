d5884b049398918ff19e4f32cd7d703bbabcaded
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-05 22:50:59 +0200

    unnecessary grid-gap removed

--- src/App/homepage/Header/styled.js
+++ src/App/homepage/Header/styled.js
@@ -2,7 +2,6 @@ import { styled } from "styled-components";
 
 const StyledHeader = styled.div`
   display: grid;
-  grid-gap: 66px;
   grid-template-columns: auto 1fr;
   margin-top: 119px;
   place-items: center;
