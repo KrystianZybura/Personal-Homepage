946c371532bffc39308060b2ff12cef27dc9abbe
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-08 19:17:53 +0200

    initial setup for Repositories created

--- src/App/homepage/Repositories/index.jsx
+++ src/App/homepage/Repositories/index.jsx
@@ -1,3 +1,19 @@
-const Repositories = () => <div></div>;
+import {
+  RepositoriesContainer,
+  RepositoriesHeader,
+  RepositoriesSubTitle,
+  RepositoriesTitle,
+} from "./styled";
+
+const Repositories = () => (
+  <>
+    <RepositoriesHeader>
+      <img src="github-logo.svg" alt="Github logo" />
+      <RepositoriesTitle>Portfolio</RepositoriesTitle>
+      <RepositoriesSubTitle>My recent projects</RepositoriesSubTitle>
+    </RepositoriesHeader>
+    <RepositoriesContainer></RepositoriesContainer>
+  </>
+);
 
 export default Repositories;
--- src/App/homepage/Repositories/styled.js
+++ src/App/homepage/Repositories/styled.js
@@ -1,1 +1,29 @@
 import { styled } from "styled-components";
+
+const RepositoriesHeader = styled.div`
+  display: grid;
+  place-items: center;
+  margin-top: 72px;
+`;
+
+const RepositoriesTitle = styled.h2`
+  font-size: 30px;
+  font-weight: 900;
+`;
+
+const RepositoriesSubTitle = styled.span`
+  font-size: 20px;
+`;
+
+const RepositoriesContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 32px;
+`;
+
+export {
+  RepositoriesContainer,
+  RepositoriesHeader,
+  RepositoriesTitle,
+  RepositoriesSubTitle,
+};
