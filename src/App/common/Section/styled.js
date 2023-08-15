e67d5a0e7b956d12f560297e70b2f1a699feb65d
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-15 23:04:36 +0200

    section adjusted

--- src/App/common/Section/styled.js
+++ src/App/common/Section/styled.js
@@ -5,7 +5,7 @@ const StyledSection = styled.section`
   box-shadow:
     0px 16px 58px 0px ${({ theme }) => theme.firstBoxShadow},
     0px -2px 50px 0px ${({ theme }) => theme.secondBoxShadow};
-  padding: 33px;
+  padding: 32px;
   margin-top: 73px;
 `;
 
@@ -13,6 +13,7 @@ const SectionHeader = styled.h2`
   font-size: 30px;
   font-weight: 900;
   padding-bottom: 15px;
+  margin: 0 0 32px 0;
   border-bottom: 1px solid ${({ theme }) => theme.border};
 `;
 
@@ -24,11 +25,13 @@ const SectionUl = styled.ul`
   list-style-image: url("src/App/assets/ellipse.svg");
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+  gap: 12px;
+  margin: 0;
+  padding-left: 15px;
 `;
 
 const SectionLi = styled.li`
-  margin: 8px;
-  padding-left: 10px;
+  padding-left: 8px;
 `;
 
 export { SectionUl, SectionLi, StyledSection, SectionHeader, SectionContainer };
