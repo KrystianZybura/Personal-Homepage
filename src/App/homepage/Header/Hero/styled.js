e6f132f6b59d48125a57a6c75c9be21ee478e96c
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-17 10:43:41 +0200

    Hero adjusted and partially refactored

--- src/App/homepage/Header/Hero/styled.js
+++ src/App/homepage/Header/Hero/styled.js
@@ -2,6 +2,10 @@ import { styled } from "styled-components";
 
 const HeroContainer = styled.div`
   max-width: 633px;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: wrap;
+  gap: 16px;
 `;
 
 const HeroImage = styled.img`
@@ -14,32 +18,30 @@ const HeroImage = styled.img`
 const HeroTitle = styled.h1`
   font-size: 38px;
   color: #000;
-  margin: 12px 0 35px;
   font-weight: 900;
+  margin: 0;
 `;
 
 const HeroAbout = styled.p`
   font-size: 20px;
-  margin-bottom: 32px;
   line-height: 28px;
   color: ${({ theme }) => theme.font};
 `;
 
 const HeroHire = styled.a`
-  background-color: ${({ theme }) => theme.primary};
-  color: #fff;
-  text-decoration: none;
-  padding: 12px 16px;
-  max-width: 154px;
-  text-align: center;
   display: flex;
   justify-content: space-evenly;
   align-items: center;
   gap: 16px;
-  font-size: 20px;
+  max-width: 154px;
+  padding: 12px 16px;
   border: 1px solid ${({ theme }) => theme.border};
   border-radius: 5px;
   font-weight: 600;
+  font-size: 20px;
+  background-color: ${({ theme }) => theme.primary};
+  color: #fff;
+  text-decoration: none;
 
   &:hover {
     box-shadow:
