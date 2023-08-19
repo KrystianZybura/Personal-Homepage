17d728826906226fffe890523bebd57c3a269799
Krystian Żybura <Krystian12324444@gmail.com>
2023-08-19 12:53:10 +0200

    media queries added, adjusted

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -15,9 +15,14 @@ const GlobalStyles = createGlobalStyle`${css`
     font-family: "Inter", sans-serif;
     background: ${({ theme }) => theme.background};
     margin: 0 auto;
-    max-width: 1216px;
+    max-width: 1232px;
+    padding: 0 16px;
     font-size: 18px;
     color: ${({ theme }) => theme.font};
+
+    @media (max-width: 767px) {
+      font-size: 14px;
+    }
   }
 `}`;
 
