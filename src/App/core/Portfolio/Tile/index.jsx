929b75f12a8003a6e4bcf445958120f209ddb082
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-19 18:51:27 +0200

    reverted previous changes. adjusted

--- src/App/core/Portfolio/Tile/index.jsx
+++ src/App/core/Portfolio/Tile/index.jsx
@@ -9,22 +9,25 @@ import {
 
 const Tile = ({ repos }) => (
   <>
-    {repos.map(({ name, description, html_url, website }) => (
-      <StyledTile key={name}>
-        <TileTitle>{name}</TileTitle>
-        <TileDescription>{description}</TileDescription>
-        <LinkContainer>
-          <LinkDescription>Demo:</LinkDescription>
-          <TileLink rel="no-referrer" target="_blank" href={website}>
-            https://link.demo.com
-          </TileLink>
-          <LinkDescription>Code:</LinkDescription>
-          <TileLink rel="no-referrer" target="_blank" href={html_url}>
-            https://link.code.com
-          </TileLink>
-        </LinkContainer>
-      </StyledTile>
-    ))}
+    {repos.map(
+      ({ name, description, html_url, homepage }) =>
+        homepage && (
+          <StyledTile key={name}>
+            <TileTitle>{name}</TileTitle>
+            <TileDescription>{description}</TileDescription>
+            <LinkContainer>
+              <LinkDescription>Demo:</LinkDescription>
+              <TileLink rel="no-referrer" target="_blank" href={homepage}>
+                https://link.demo.com
+              </TileLink>
+              <LinkDescription>Code:</LinkDescription>
+              <TileLink rel="no-referrer" target="_blank" href={html_url}>
+                https://link.code.com
+              </TileLink>
+            </LinkContainer>
+          </StyledTile>
+        ),
+    )}
   </>
 );
 
