bbbe2056f4c5d32c28242f7e097a1d756fa3bc4d
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-20 09:04:38 +0200

    getTheme transformed into useTheme

---
+++ src/App/useTheme.js
@@ -0,0 +1,29 @@
+import { useContext } from "react";
+
+import { ThemeContext } from "../ThemeStatus";
+
+const useTheme = () => {
+  const darkTheme = useContext(ThemeContext);
+
+  return {
+    styles: {
+      background: darkTheme ? "#252525" : "#FBFBFE",
+      fontSecondary: darkTheme ? "#FFFFFF" : "#6E7E91",
+      font: darkTheme ? "#FFFFFF" : "#252525",
+      primary: darkTheme ? "#2188FF" : "#0366D6",
+      sectionBackground: darkTheme ? "#363636B8" : "#FFFFFF",
+      border: darkTheme ? "#D1D5DA1A" : "#D1D5DA4D",
+      borderHover: darkTheme ? "#0366D680" : "#0366D633",
+      firstBoxShadow: "#090A3308",
+      secondBoxShadow: "#090A3305",
+      buttonBoxShadow: darkTheme ? "#6D93BE" : "#8CC2FF",
+    },
+    breakpoints: {
+      laptop: "1200px",
+      tablet: "767px",
+      mobile: "550px",
+    },
+  };
+};
+
+export { useTheme };
