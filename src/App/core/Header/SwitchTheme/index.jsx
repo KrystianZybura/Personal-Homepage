9b08be363884a1542eb0355d0d22945278a3b222
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-23 13:36:12 +0200

    adjusted and corrected dark mode status

--- src/App/core/Header/SwitchTheme/index.jsx
+++ src/App/core/Header/SwitchTheme/index.jsx
@@ -3,7 +3,7 @@ import { useContext } from "react";
 import { ThemeContext, ThemeUpdateContext } from "../../../../ThemeStatus";
 import { ReactComponent as DarkThemeIcon } from "../../../assets/dark.svg";
 import { ReactComponent as LightThemeIcon } from "../../../assets/light.svg";
-import { Initiation } from "../../../common/Initiation";
+import EnhancedData from "../../../common/EnhancedData";
 import { SwitchThemeButton, SwitchThemeContainer } from "./styled";
 
 const SwitchTheme = () => {
@@ -12,7 +12,7 @@ const SwitchTheme = () => {
 
   return (
     <SwitchThemeContainer>
-      <Initiation>{`Dark mode off`}</Initiation>
+      <EnhancedData heading={`Dark mode ${darkTheme ? "on" : "off"}`} />
       <SwitchThemeButton onClick={() => toggleTheme()}>
         {darkTheme ? <DarkThemeIcon /> : <LightThemeIcon />}
       </SwitchThemeButton>
