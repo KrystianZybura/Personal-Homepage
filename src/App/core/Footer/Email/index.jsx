e677120443d5e2a0264c3539bb7518e9e0048afe
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-23 13:33:57 +0200

    Email added

---
+++ src/App/core/Footer/Email/index.jsx
@@ -0,0 +1,9 @@
+import { StyledEmail } from "./styled";
+
+const Email = () => (
+  <StyledEmail href="mailto:zybura.dev@gmail.com">
+    zybura.dev@gmail.com
+  </StyledEmail>
+);
+
+export default Email;
---
+++ src/App/core/Footer/Email/styled.js
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+export const StyledEmail = styled.a`
+  font-size: 32px;
+  font-weight: 900;
+  justify-self: start;
+  text-decoration: none;
+  color: inherit;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 18px;
+  }
+
+  &:hover {
+    color: ${({ theme }) => theme.styles.primary};
+  }
+`;
