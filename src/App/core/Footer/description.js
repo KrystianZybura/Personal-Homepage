05dee6d368f8767a4ebf3e7704f10bd1724a3ad0
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-23 13:33:29 +0200

    description added

---
+++ src/App/core/Footer/description.js
@@ -0,0 +1,1 @@
+export const description = `I'm always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas com to life, feel free to contact me ✌`;
