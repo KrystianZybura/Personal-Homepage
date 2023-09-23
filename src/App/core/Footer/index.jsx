06de2494f39a17effdf0e3ca6d3e0b705b74879d
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-23 13:33:47 +0200

    Footer adjusted

--- src/App/core/Footer/index.jsx
+++ src/App/core/Footer/index.jsx
@@ -1,35 +1,18 @@
-import { Initiation } from "../../common/Initiation";
-import FooterLink from "./FooterLink";
-import {
-  FacebookIcon,
-  FooterDescription,
-  FooterEmail,
-  FooterSocials,
-  GithubIcon,
-  LinkedInIcon,
-  StyledFooter,
-} from "./styled";
+import EnhancedData from "../../common/EnhancedData";
+import Email from "./Email";
+import Socials from "./Socials";
+import { description } from "./description";
+import { StyledFooter } from "./styled";
 
 const Footer = () => (
   <StyledFooter>
-    <Initiation>Lets talk!</Initiation>
-    <FooterEmail href="mailto:zybura.dev@gmail.com">
-      zybura.dev@gmail.com
-    </FooterEmail>
-    <FooterDescription>
-      {`I'm always open to new projects whenever I have the time. If you have a website, dashboard or mobile app in mind and need some help to make your ideas com to life, feel free to contact me ✌`}
-    </FooterDescription>
-    <FooterSocials>
-      <FooterLink href="https://github.com/KrystianZybura">
-        <GithubIcon />
-      </FooterLink>
-      <FooterLink href="https://www.facebook.com/168299279807k">
-        <FacebookIcon />
-      </FooterLink>
-      <FooterLink href="https://www.linkedin.com/in/krystian-żybura-062929288/">
-        <LinkedInIcon />
-      </FooterLink>
-    </FooterSocials>
+    <EnhancedData
+      heading="Let's talk!"
+      title={<Email />}
+      description={description}
+      socials={<Socials />}
+      footer
+    />
   </StyledFooter>
 );
 
--- src/App/core/Footer/styled.js
+++ src/App/core/Footer/styled.js
@@ -1,9 +1,5 @@
 import { styled } from "styled-components";
 
-import { ReactComponent as Facebook } from "../../assets/facebook.svg";
-import { ReactComponent as Github } from "../../assets/github.svg";
-import { ReactComponent as LinkedIn } from "../../assets/linkedIn.svg";
-
 export const StyledFooter = styled.footer`
   display: grid;
   grid-template-columns: 1fr;
@@ -15,47 +11,3 @@ export const StyledFooter = styled.footer`
     margin: 48px 0 31px;
   }
 `;
-
-export const FooterEmail = styled.a`
-  font-size: 32px;
-  font-weight: 900;
-  justify-self: start;
-  text-decoration: none;
-  color: inherit;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    font-size: 18px;
-  }
-
-  &:hover {
-    color: ${({ theme }) => theme.styles.primary};
-  }
-`;
-
-export const FooterDescription = styled.p`
-  max-width: 670px;
-  margin: 0;
-`;
-
-export const FooterSocials = styled.div`
-  display: flex;
-  gap: 24px;
-  margin-top: 32px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    gap: 16px;
-    margin-top: 24px;
-  }
-`;
-
-export const createFooterIcon = (svg) => styled(svg)`
-  width: 48px;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
-    width: 32px;
-  }
-`;
-
-export const FacebookIcon = createFooterIcon(Facebook);
-export const GithubIcon = createFooterIcon(Github);
-export const LinkedInIcon = createFooterIcon(LinkedIn);
