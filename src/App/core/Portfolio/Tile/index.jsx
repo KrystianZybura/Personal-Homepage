a58cc11a220e024c06cbbe37b9d88cb318d818de
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-27 19:57:21 +0200

    Tile title modified, refactored

--- src/App/core/Portfolio/Tile/index.jsx
+++ src/App/core/Portfolio/Tile/index.jsx
@@ -7,28 +7,27 @@ import {
   Title,
 } from "./styled";
 
-const Tile = ({ repos }) => (
-  <>
-    {repos.map(
-      ({ name, description, html_url, homepage }) =>
-        homepage && (
-          <TileWrapper key={name}>
-            <Title>{name}</Title>
-            <Description>{description}</Description>
-            <LinksContainer>
-              <LinkDescription>Demo:</LinkDescription>
-              <Link rel="no-referrer" target="_blank" href={homepage}>
-                https://link.demo.com
-              </Link>
-              <LinkDescription>Code:</LinkDescription>
-              <Link rel="no-referrer" target="_blank" href={html_url}>
-                https://link.code.com
-              </Link>
-            </LinksContainer>
-          </TileWrapper>
-        ),
-    )}
-  </>
-);
+const Tile = ({ repos }) =>
+  repos.map(
+    ({ name, description, html_url, homepage }) =>
+      homepage && (
+        <TileWrapper key={name}>
+          <Title>
+            {name[0].toUpperCase() + name.substring(1).replaceAll("-", " ")}
+          </Title>
+          <Description>{description}</Description>
+          <LinksContainer>
+            <LinkDescription>Demo:</LinkDescription>
+            <Link rel="no-referrer" target="_blank" href={homepage}>
+              https://link.demo.com
+            </Link>
+            <LinkDescription>Code:</LinkDescription>
+            <Link rel="no-referrer" target="_blank" href={html_url}>
+              https://link.code.com
+            </Link>
+          </LinksContainer>
+        </TileWrapper>
+      ),
+  );
 
 export default Tile;
