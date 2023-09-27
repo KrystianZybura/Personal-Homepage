0a6a4934e12809458eb989bf9e09af3039960e2b
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-27 20:20:11 +0200

    Error component design improved

--- src/App/core/Portfolio/Error/styled.js
+++ src/App/core/Portfolio/Error/styled.js
@@ -1,10 +1,16 @@
 import { styled } from "styled-components";
 
+import { ReactComponent as icon } from "../../../assets/danger.svg";
+
 export const ErrorWrapper = styled.div`
   text-align: center;
-  margin-bottom: 68px;
+  margin: 60px 0 68px;
 
   @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    margin: 20px 0 20px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
     margin: 0;
   }
 `;
@@ -12,14 +18,37 @@ export const ErrorWrapper = styled.div`
 export const ErrorHeading = styled.h3`
   font-size: 24px;
   font-weight: 700;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 20px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 18px;
+  }
 `;
 
 export const ErrorMessage = styled.p`
   font-size: 20px;
   line-height: 28px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: unset;
+  }
+`;
+
+export const ErrorIcon = styled(icon)`
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    width: 36px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    width: 30px;
+  }
 `;
 
 export const EmergencyLink = styled.a`
+  display: inline-block;
   padding: 12px 16px;
   border: 1px solid ${({ theme }) => theme.styles.border};
   border-radius: 5px;
@@ -28,8 +57,17 @@ export const EmergencyLink = styled.a`
   background-color: ${({ theme }) => theme.styles.primary};
   color: #fff;
   text-decoration: none;
-  display: inline-block;
   max-width: 168px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}) {
+    font-size: 18px;
+    padding: 10px 14px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}) {
+    font-size: 16px;
+    padding: 8px 12px;
+  }
 
   &:hover {
     box-shadow:
