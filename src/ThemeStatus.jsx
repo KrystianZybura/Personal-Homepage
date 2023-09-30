d8e67d1a886cb702223fb1b94fb48594ee074fd8
Krystian Żybura <Krystian12324444@gmail.com>
2023-09-30 18:25:22 +0200

    dark theme status added to local storage

--- src/ThemeStatus.jsx
+++ src/ThemeStatus.jsx
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export const ThemeContext = React.createContext();
 export const ThemeUpdateContext = React.createContext();
 
 const ThemeStatus = ({ children }) => {
-  const [darkTheme, setDarkTheme] = useState(false);
+  const [darkTheme, setDarkTheme] = useState(
+    JSON.parse(localStorage.getItem("darkThemeStatus")) ?? false,
+  );
+
+  useEffect(() => {
+    localStorage.setItem("darkThemeStatus", JSON.stringify(darkTheme));
+  }, [darkTheme]);
 
   const toggleTheme = () => setDarkTheme((darkTheme) => !darkTheme);
 
