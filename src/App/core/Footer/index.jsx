76c3322797bee8bb3fb9cc09d4375e5b97f229db
Krystian Żybura <Krystian12324444@gmail.com>
2023-10-29 01:20:52 +0200

    Footer adjusted

--- src/App/core/Footer/index.jsx
+++ src/App/core/Footer/index.jsx
@@ -1,16 +1,16 @@
-import EnhancedData from "../../common/EnhancedData";
+import { Heading } from "../../common/Heading";
 import { FOOTER_DESCRIPTION } from "../../common/textConstants";
 import Email from "./Email";
 import Socials from "./Socials";
+import { Description, FooterWrapper } from "./styled";
 
 const Footer = ({ heading }) => (
-  <EnhancedData
-    heading={heading}
-    title={<Email />}
-    description={FOOTER_DESCRIPTION}
-    socials={<Socials />}
-    footer
-  />
+  <FooterWrapper>
+    <Heading>{heading}</Heading>
+    <Email />
+    <Description>{FOOTER_DESCRIPTION}</Description>
+    <Socials />
+  </FooterWrapper>
 );
 
 export default Footer;
---
+++ src/App/core/Footer/styled.js
@@ -0,0 +1,37 @@
+import { styled } from "styled-components";
+
+export const FooterWrapper = styled.footer`
+  max-width: 633px;
+  display: flex;
+  flex-flow: column wrap;
+  gap: 32px;
+  margin-top: 48px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    gap: 24px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    gap: 12px;
+    margin: 0;
+  }
+`;
+
+export const Description = styled.p`
+  margin: 0;
+  font-size: 20px;
+  line-height: 28px;
+  color: ${({ theme }) => theme.styles.fontSecondary};
+  max-width: 670px;
+  margin: 0;
+  line-height: inherit;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tablet}px) {
+    font-size: 19px;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobile}px) {
+    font-size: 17px;
+    line-height: 24px;
+  }
+`;
