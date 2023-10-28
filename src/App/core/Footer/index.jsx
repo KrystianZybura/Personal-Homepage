fbce2029d55b228bbb8341bc82f566cb0c9cc829
Krystian Żybura <Krystian12324444@gmail.com>
2023-10-29 01:23:06 +0200

    Email element simplified

--- src/App/core/Footer/index.jsx
+++ src/App/core/Footer/index.jsx
@@ -1,13 +1,16 @@
 import { Heading } from "../../common/Heading";
+import Link from "../../common/Link";
+import { email } from "../../common/email";
 import { FOOTER_DESCRIPTION } from "../../common/textConstants";
-import Email from "./Email";
 import Socials from "./Socials";
 import { Description, FooterWrapper } from "./styled";
 
 const Footer = ({ heading }) => (
   <FooterWrapper>
     <Heading>{heading}</Heading>
-    <Email />
+    <Link email href={`mailto:${email}`}>
+      {email}
+    </Link>
     <Description>{FOOTER_DESCRIPTION}</Description>
     <Socials />
   </FooterWrapper>
