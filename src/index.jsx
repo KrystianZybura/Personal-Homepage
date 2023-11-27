eb7b61a697d0437da81597b76cd70ee139317fe5
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-27 23:26:47 +0100

    react query installed

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 import App from "./App";
 import ThemeStatus from "./ThemeStatus";
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ThemeStatus>
-      <App />
-    </ThemeStatus>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
+      <ThemeStatus>
+        <App />
+      </ThemeStatus>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
