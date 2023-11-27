0090e9515b7b1d82aef787d4611764d5856ffbb2
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-27 23:33:13 +0100

    additional timeout added for loading to be visible

--- src/App/core/Portfolio/fetchReposData.js
+++ src/App/core/Portfolio/fetchReposData.js
@@ -1,6 +1,10 @@
 const REPOS_API_URL = "https://api.github.com/users/krystianzybura/repos";
 
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
 export const fetchReposData = async () => {
+  await wait(300);
+
   const response = await fetch(REPOS_API_URL);
 
   return await response.json();
