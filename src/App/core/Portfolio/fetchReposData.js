58802c31850214371f4ae28e4415c0c6ec12ec90
Krystian Żybura <Krystian12324444@gmail.com>
2023-11-29 17:38:45 +0100

    in case of error, error message added

--- src/App/core/Portfolio/fetchReposData.js
+++ src/App/core/Portfolio/fetchReposData.js
@@ -1,4 +1,4 @@
-const REPOS_API_URL = "https://api.github.com/users/krystianzybura/repos";
+const REPOS_API_URL = "https://api.github.com/users/kry stianzybura/repos";
 
 const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -6,6 +6,10 @@ export const fetchReposData = async () => {
   await wait(300);
 
   const response = await fetch(REPOS_API_URL);
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch repositories.");
+  }
 
   return await response.json();
 };
